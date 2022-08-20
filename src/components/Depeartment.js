3f1d8a271eb04f2ca061b3766be19be94ece843e
Raman Sharma <rksraman73@gmail.com>
2022-08-20 21:15:21 +0530

    h2 heading

--- src/components/Depeartment.js
+++ src/components/Depeartment.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 export const Depeartment   = () => {
   return (
-    <div>Depeartment  component  </div>
+    <div>Depeartment  component
+         <h2>bduedl </h2>  </div>
+
   )
 }
 
