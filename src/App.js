5bd32b8bba6cd66713b3efa20f09143ad9884705
Raman Sharma <rksraman73@gmail.com>
2022-08-21 11:33:32 +0530

    Routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import react , {useState} from 'react'
-import { BrowserRouter , Route, Routes } from 'react-router-dom';
-import './App.css';
-import { Depeartment } from './components/Depeartment';
-import { Student } from './components/Student';
+import react, { useState } from "react";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Depeartment } from "./components/Depeartment";
+import { Student } from "./components/Student";
 
 function App() {
-  const hlo=()=>{
-    alert("Welcome to Star Health...")
-}
+  const hlo = () => {
+    alert("Welcome to Star Health...");
+  };
 
   return (
-    <div className="App">
+    <>
       <h1>Welcome To React Project</h1>
-      
-    <h1><button onClick={hlo}>Event</button></h1>
-      <Depeartment/>
-      <Student/>
-    
-    </div>
+      <h1>
+        <button onClick={hlo}>Event</button>
+      </h1>
+
+      <ul>
+        <li>
+          <Link to="/student">Student</Link>
+        </li>
+        <li>
+          <Link to="/departement">Depeartment</Link>
+        </li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        
+      </ul>
+
+      <Routes>
+        <Route exect path="/" />
+      </Routes>
+      <Routes>
+        <Route path="/student" element={<Student />} />
+      </Routes>
+      <Routes>
+        <Route path="/departement" element={<Depeartment />} />
+      </Routes>
+    </>
   );
 }
 
